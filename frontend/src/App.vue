efdf7353ad7fac83c689db3d73925e1c9fb8756d
Bryan Barrows <bryan.barrows@seattlecolleges.edu>
2018-06-06 23:56:35 -0700

    finished axios settings

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -26,8 +26,45 @@
 </template>
 
 <script>
+import AuthService from './auth/AuthService'
+import axios from 'axios'
+
+const API_URL = 'http://localhost:8000'
+const auth = new AuthService()
+
 export default {
-  name: 'App'
+  name: 'app',
+  data () {
+    this.handleAuthentication()
+    this.authenticated = false
+
+    auth.authNotifier.on('authChange', authState => {
+      this.authenticated = authState.authenticated
+    })
+
+    return {
+      authenticated: false,
+      message: ''
+    }
+  },
+  methods: {
+    login () {
+      auth.login()
+    },
+    handleAuthentication () {
+      auth.handleAuthentication()
+    },
+    logout () {
+      auth.logout()
+    },
+    privateMessage () {
+      const url = `${API_URL}/api/private/`
+      return axios.get(url, {headers: {Authorization: `Bearer ${AuthService.getAuthToken()}`}}).then((response) => {
+        console.log(response.data)
+        this.message = response.data || ''
+      })
+    }
+  }
 }
 </script>
 
